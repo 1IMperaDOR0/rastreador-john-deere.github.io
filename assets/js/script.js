c2bd37e098b13b737b758bf872f9aebf1d2ae459
1MperaDOR0 <lucaslucore@gmail.com>
2024-08-26 19:42:55 -0300

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-let menu = document.querySelector(".menu")
-menu.addEventListener("click", () => {
+let menu = document.querySelector(".menu").addEventListener("click", () => {
     let nav = document.querySelector('nav')
     nav.style.display = 'block'
     menu.style.display = 'none'
 })
 
-let close = document.querySelector(".close")
-close.addEventListener("click", () => {
+let close = document.querySelector(".close").addEventListener("click", () => {
     let nav = document.querySelector('nav')
     nav.style.display = 'none'
     menu.style.display = 'block'
